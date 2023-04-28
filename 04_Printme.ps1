9f00141743b3d9fc0275d87214840f20f2c70cae
John Jeffrey PANINGBATAN <53562492+Muneo42@users.noreply.github.com>
2023-04-28 12:11:31 +0200

    Update 04_Printme.ps1

--- 04_Printme.ps1
+++ 04_Printme.ps1
@@ -2,13 +2,10 @@
 Start-Process C:\script_folder\printme
 
 # Pause the script to ensure the drivers are installed
-Start-Sleep -Seconds 60
+Start-Sleep -Seconds 15
 
 # Set the name of the printer and the network path
 $printerName = "\\106.102.1.90\printme"
-
-# Download the drivers of the printer
-& $printerName
 
 # Set the printer as default
 Set-ItemProperty -Path "HKCU:\Software\Microsoft\Windows NT\CurrentVersion\Windows" -Name "Device" -Value $printerName
