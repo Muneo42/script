0b28c7e81019d5a5bf1418dd2cfb8a12e696635f
John Jeffrey PANINGBATAN <53562492+Muneo42@users.noreply.github.com>
2023-04-28 12:11:01 +0200

    Delete 05_Teams.ps1

--- 05_Teams.ps1
+++
@@ -1,4 +0,0 @@
-cd C:\script_folder
-& .\TeamsSetup_c_w_.exe
-Start-Process -FilePath .\KnoxMeeting_Install_net4.exe -ArgumentList "/silent /install" -Wait
-Start-Process -FilePath .\KnoxPortalSetup.exe -ArgumentList "/silent /install" -Wait
