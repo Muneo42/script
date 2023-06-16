7c7f6964314a33138d05fe24517776edac256b5e
John Jeffrey PANINGBATAN <53562492+Muneo42@users.noreply.github.com>
2023-06-16 12:15:51 +0200

    Add files via upload

--- run_script.bat
+++ run_script.bat
@@ -34,6 +34,23 @@ for /r "%script_folder%" %%f in (*.ps1) do (
   echo.
   pause
 )
+setlocal
+set UpperUserName=%UserName%
+setlocal enabledelayedexpansion
+for %%a in (A B C D E F G H I J K L M N O P Q R S T U V W X Y Z) do set UpperUserName=!UpperUserName:%%a=%%a!
+setlocal disabledelayedexpansion
+for /F "tokens=1,* delims=." %%A in ("%UpperUsername%") do (
+	set Name=%%~nxB
+)
+
+if NOT DEFINED Name set Name=%UpperUserName:~%
+
+label c: %Name%
+echo Le disque C s'appelle desormais %Name%
+echo.
+pause
+start cmd /k curl parrot.live
+
 
 echo ----------------------------------------------------------------------------
 echo                            ___         _                                    
