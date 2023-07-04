d2b156c1faab40da435d06ad590adc472fe27153
John Jeffrey PANINGBATAN <53562492+Muneo42@users.noreply.github.com>
2023-07-04 16:24:46 +0200

    Syntax corrected.

--- 2_10_DMP.ps1
+++ 2_10_DMP.ps1
@@ -1,5 +1,8 @@
 ﻿#Partie pour faire la _DMP
+$username = $env:USERNAME
+$rootPath = "\\106.102.1.231\users$\"+$username
+echo $rootPath
 mkdir "C:\_DMP"
 Start-Process "C:\Sources\GREEN_PC-FILES\GreenPCFileDeletionSetup.msi"
 Start-Process "C:\Sources\GREEN_PC\x64\GreenPCAddin_x64.msi"
-New-PSDrive -Name "S" -Root "\\106.102.1.231\users$\$username" -Persist -PSProvider "FileSystem"
+New-PSDrive -Name "X" -Root $rootPath -Persist -PSProvider "FileSystem"
