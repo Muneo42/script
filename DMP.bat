89bb77f0d17c4c04e40d7fbfced82d23dbaae1cc
John Jeffrey PANINGBATAN <53562492+Muneo42@users.noreply.github.com>
2023-07-20 14:43:36 +0200

    Update DMP.bat

--- DMP.bat
+++ DMP.bat
@@ -39,9 +39,6 @@ set UpperUserName=%UserName%
 setlocal enabledelayedexpansion
 for %%a in (A B C D E F G H I J K L M N O P Q R S T U V W X Y Z) do set UpperUserName=!UpperUserName:%%a=%%a!
 setlocal disabledelayedexpansion
-for /F "tokens=1,* delims=." %%A in ("%UpperUsername%") do (
-	set Name=%%~nxB
-)
 
 if NOT DEFINED Name set Name=%UpperUserName:~%
 
